ccfc18208733f4569bbea1f23131b7b392dab150
AmieAnne <142542738+AmieAnne@users.noreply.github.com>
2023-09-18 20:29:11 -0500

    Update string-modification.js

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,21 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
+let str2 = "Launchcode"
+let str3 = "nchCodeLau"
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+console.log(str.slice(3)+str.slice(0,3))
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
-
+console.log("Str has " + str.length + " characters. Str 2 has " + str3.length + " characters.")
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
-//3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+let answer = 3
+let question = Number(input.question("How many characters were effected? "));
+this.answer
+if (question == answer) {
+    console.log("YES! Good job! 😍");
+  }
+  else if (question != answer){
+    console.log("No! Sorry. 😓");
+  }
