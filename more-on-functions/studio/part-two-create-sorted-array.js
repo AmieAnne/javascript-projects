c7e128d8cb32f613c5d8d2a1e0e8360dbe420e88
AmieAnne <142542738+AmieAnne@users.noreply.github.com>
2023-09-28 19:48:52 -0500

    Update part-two-create-sorted-array.js

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,3 +1,4 @@
+const input = require('readline-sync');
 function findMinValue(arr){
   let min = arr[0];
   for (i = 0; i < arr.length; i++){
@@ -8,8 +9,9 @@ function findMinValue(arr){
   return min;
 }
 
-//Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
-
+//Create a function with an array of numbers as its parameter. This function will return a new array with 
+//the numbers sorted from least to greatest value.
+// 2nd parameter means remove one item only
 /*Within the function:
 1) Define a new, empty array to hold the final sorted numbers.
 2) Use the findMinValue function to find the minimum value in the old array.
@@ -19,7 +21,15 @@ function findMinValue(arr){
 6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
-
+function sortArray(arr) {
+  let sortedArr = []
+  while (arr.length > 0) {
+    let minValue = findMinValue(arr) 
+    sortedArr.push(minValue)
+    let index = arr.indexOf(minValue)
+arr.splice(index, 1);  }
+return sortedArr
+}
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
 
@@ -27,3 +37,6 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+console.log(sortArray(nums1))
+console.log(sortArray(nums2))
+console.log(sortArray(nums3))
